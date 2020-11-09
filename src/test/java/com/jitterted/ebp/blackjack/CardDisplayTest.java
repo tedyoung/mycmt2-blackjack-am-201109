3d10f23c97f386a5bc5f543e51353f5107979d43
Ted M. Young <tedyoung@gmail.com>
2020-11-09 08:46:45 -0800

    Added characterization tests for Card's display method.

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,27 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CardDisplayTest {
+
+  private static final Suit DUMMY_SUIT = Suit.CLUBS;
+
+  @Test
+  public void displayTenAsString() throws Exception {
+    Card card = new Card(DUMMY_SUIT, Rank.TEN);
+
+    assertThat(card.display())
+        .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│10       │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│       10│\u001B[1B\u001B[11D└─────────┘");
+  }
+
+  @Test
+  public void displayNonTenAsString() throws Exception {
+    Card card = new Card(DUMMY_SUIT, Rank.FIVE);
+
+    assertThat(card.display())
+        .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│5        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        5│\u001B[1B\u001B[11D└─────────┘");
+  }
+  
+}
