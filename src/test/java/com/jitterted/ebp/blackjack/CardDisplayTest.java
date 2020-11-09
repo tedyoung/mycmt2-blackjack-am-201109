37ea82fa2900d351a0b7d7a36faf7563324fa15b
Ted M. Young <tedyoung@gmail.com>
2020-11-09 09:03:04 -0800

    Moved Card's display method to ConsoleCard.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -12,7 +12,7 @@ public class CardDisplayTest {
   public void displayTenAsString() throws Exception {
     Card card = new Card(DUMMY_SUIT, Rank.TEN);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
         .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│10       │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│       10│\u001B[1B\u001B[11D└─────────┘");
   }
 
@@ -20,7 +20,7 @@ public class CardDisplayTest {
   public void displayNonTenAsString() throws Exception {
     Card card = new Card(DUMMY_SUIT, Rank.FIVE);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
         .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│5        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        5│\u001B[1B\u001B[11D└─────────┘");
   }
   
